7e558d29b974eb647d7b46d5c8c6942606340bdb
jhhfft <yan358941877@163.com>
2018-08-19 20:42:24 +0800

    主页面样式

---
+++ public/employee-info/src/Main/Content/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+class Content extends React.Component{
+  render(){
+    return (
+      <div className="content">
+        This is content
+      </div>
+    )
+  }
+}
+
+export default Content;
--- public/employee-info/src/Main/index.js
+++ public/employee-info/src/Main/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import AddEmployee from './Container/AddEmployee';
-import DetailEmployee from './Container/DetailEmployee';
-import ModifyEmployee from './Container/ModifyEmployee';
-import QueryEmployee from './Container/QueryEmployee';
+
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+import Content from './Content';
+
+import './index.css';
 
 class Main extends React.Component{
     render(){
       return (
-        <div>
-          <h1>This is Main</h1>
-          <Switch>
-            <Route path="/main/add" component={AddEmployee}></Route>
-            <Route path="/main/detail" component={DetailEmployee}></Route>
-            <Route path="/main/modify" component={ModifyEmployee}></Route>
-            <Route path="/main/query" component={QueryEmployee}></Route>
-          </Switch>
+        <div className="main">
+          <Header />
+            <div className="wrapper">
+              <Content />
+            </div>
+          {/* <Footer /> */}
         </div>
       )
     }
